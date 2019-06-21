afcf36cc423ad7ba0bfd21333da54022c5d99d70
Abdellah Ramadan <ajrmdn@gmail.com>
2019-06-21 10:00:20 -0700

    Add objects and prototypes to scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,33 @@
-// Business Logic //
+// Business Logic for PizzaPlace //
 
 //--- This is a CONSTRUCTOR ---//
 function PizzaPlace() {
   this.toppings = [],
-  this.size = []
+  this.sizes = []
 }
 
+//--- This is a PROTOTYPE ---//
 PizzaPlace.prototype.addTopping = function(topping) {
-  topping.id = this.assignId();
   this.toppings.push(topping);
-}  
+}
+
+PizzaPlace.prototype.addSize = function(size) {
+  this.sizes.push(size);
+}
+
+// Business Logic for Toppings //
+
+//--- This is an OBJECT ---//
+function Toppings(firstTopping, secondTopping, thirdTopping, fourthTopping, fifthTopping){
+  this.firstTopping = firstTopping,
+  this.secondTopping = secondTopping,
+  this.thirdTopping = thirdTopping,
+  this.fourthTopping = fourthTopping,
+  this.fifthTopping = fifthTopping,
+}
+
+function Sizes(small, medium, large) {
+  this.small= small,
+  this.medium = medium,
+  this.large = large,
+}
