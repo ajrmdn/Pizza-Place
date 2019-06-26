6a1a710b16c36d0a3c4afc627e8f64fdb9539ee5
Abdellah Ramadan <ajrmdn@gmail.com>
2019-06-26 12:51:49 -0700

    Add missing code to make my prototypes function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,50 @@
 // Business Logic for PizzaPlace //
 
 //--- This is a CONSTRUCTOR ---//
-function PizzaPlace() {
-  this.toppings = []
-  this.size = []
+function PizzaOrder() {
+  this.pizza = []
+}
+//--- This is an OBJECT ---//
+function Pizza(size,topping) {
+  this.size = size;
+  this.topping = topping;
 }
 
 //--- These are PROTOTYPES ---//
-PizzaPlace.prototype.addTopping = function(topping) {
-  this.toppings.push(topping);
+PizzaOrder.prototype.addPizza = function(Pizza) {
+  this.pizza.push(Pizza);
 }
 
-PizzaPlace.prototype.addSize = function(size) {
-  this.size.push(size);
+Pizza.prototype.pizzaPrice = function() {
+  if (this.size === "Small") {
+    return (10 + this.topping.length)
+  } else if (this.size === "Medium") {
+    return (15 + this.topping.length)
+  } else {
+    return (20 + this.topping.length)
+  }
 }
-
-// Business Logic for Toppings and Sizes //
-
-//--- This is an OBJECT ---//
-function Pizza(toppings, size){
-  this.toppings = toppings
-  this.size = size
-}
-
 
 // User Interface Logic for Pizza Place //
 
-var pizzaPlace = new PizzaPlace();
-
-function displayPrice(pizzaPlaceToDisplay) {
-  var pizzaPrice = $("form#pizza");
-};
-
-
+var pizzaOrder = new PizzaOrder();
 $(document).ready(function() {
-  $("#pizza").submit(function(event) {
+  $("#pizzaPie").submit(function(event) {
     event.preventDefault();
+
     //--- These are PROPERTIES ---//
-    var small = { name: "Small", price: 10};
-    var medium = { name: "Medium", price: 15};
-    var large = { name: "Large", price: 25};
-    var artichoke = { name: "Artichoke", price: 1};
-    var olives = { name: "Olives", price: 1};
-    var jalepeno = { name: "Jalepeno", price: 1};
-    var tomato = { name: "Tomato", price: 1};
-    var cheese = { name: "Cheese", price: 1};
-
-    var pizzaToppings = { name: "Pizza Toppings", toppings: [artichoke, olives, jalepeno, tomato, cheese]};
-
-    var pizzaSize = { name: "Pizza Size", size: [small, medium, large] };
-
-    $('#pizza').submit(function() {
-      $(".output").show();
-    });
+    var sizeInput = $('#pizzaPie').val();
+    var toppingsPicked = []
+    var toppingInput =
+      $('input:checkbox[name = numberOfToppings]:checked').each(function() {
+        var checkedBoxes = $(this).val();
+        totalToppings.push(' ' + checkedBoxes);
+      });
+    var newPizzaOrder = new PizzaOrder(sizeInput, toppingsPicked);
+    PizzaOrder.addpizza(newPizzaOrder);
+    var totalPrice = pizzaOrder.pizza[0].pizzaPrice();
+    $('.output').show();
+    $('#payment').text('$ ' + totalPrice + '.00');
+    $('#pizzaMade').text(sizeInput + ' pizza with ' + totalToppings);
+   });
+ });
