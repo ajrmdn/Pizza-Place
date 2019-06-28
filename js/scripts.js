6275cb5b6c7ae2292ab8b67ffdf7905ef9297626
Abdellah Ramadan <ajrmdn@gmail.com>
2019-06-27 23:06:52 -0700

    Add variables and correct syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,48 @@
 // Business Logic for PizzaPlace //
 
-// //--- This is a CONSTRUCTOR ---//
-// function PizzaOrder() {
-//   this.pizza = []
-// }
+//--- This is the OBJECT ---//
+const pizzaOrder = new Pizza();
 //--- This is a CONSTRUCTOR ---//
-function Pizza(size,topping) {
-  this.size = size;
-  this.topping = topping;
+function Pizza(size, topping1, topping2, topping3, topping4, topping5) {
+//--- These are PROPERTIES ---//
+  this.size = size,
+  this.topping1 = topping1,
+  this.topping2 = topping2,
+  this.topping3 = topping3,
+  this.topping4 = topping4,
+  this.topping5 = topping5
 }
 
-//--- These are PROTOTYPES ---//
-// PizzaOrder.prototype.addPizza = function(Pizza) {
-//   this.pizza.push(Pizza);
-// }
-
-Pizza.prototype.pizzaPrice = function() {
-  if (this.size === "Small") {
-    return (10 + this.topping.length)
-  } else if (this.size === "Medium") {
-    return (15 + this.topping.length)
-  } else {
-    return (20 + this.topping.length)
-  }
+//--- This is PROTOTYPE (algorithm C = B + A)--- pizzaPrice = method
+Pizza.prototype.pizzaPrice = function(size, topping1, topping2, topping3, topping4, topping5) {
+  const totalPrice = size + topping1 + topping2 + topping3 + topping4 + topping5
+  return totalPrice
 }
 
 // User Interface Logic for Pizza Place //
 
-var pizzaOrder = new PizzaOrder();
 $(document).ready(function() {
   $("#pizza").submit(function(event) {
     event.preventDefault();
+//--- These are USER INPUT ---//
+    const sizeInput = parseInt($('#pieSize').val());
+    const artichoke = parseInt($('input:checkbox[name=artichokes]:checked').val());
+    const olive = parseInt($('input:checkbox[name=olives]:checked').val());
+    const jalapeno = parseInt($('input:checkbox[name=jalapenos]:checked').val());
+    const tomato = parseInt($('input:checkbox[name=tomatoes]:checked').val());
+    const cheese = parseInt($('input:checkbox[name=cheese]:checked').val());
+    console.log(sizeInput);
+    console.log(artichoke);
+    console.log(olive);
+    console.log(jalapeno);
+    console.log(tomato);
+    console.log(cheese);
+//--- Calls on the METHOD ---//
+    const total = pizzaOrder.pizzaPrice(sizeInput, artichoke, olive, jalapeno, tomato, cheese);
 
-    //--- These are PROPERTIES ---//
-    var sizeInput = $('#pieSize').val();
-    // toppings picked should contain an array of all the toppings that the user picked 
-    var toppingsPicked = []
-    var toppingInput =
-      $('input:checkbox[name = numberOfToppings]:checked').each(function() {
-        var checkedBoxes = $(this).val();
-        totalToppings.push(' ' + checkedBoxes);
-      });
-    var newPizzaOrder = new PizzaOrder(sizeInput, toppingsPicked);
-    PizzaOrder.addpizza(newPizzaOrder);
-    var totalPrice = pizzaOrder.pizza[0].pizzaPrice();
+
+
     $('.output').show();
-    $('#payment').text('$ ' + totalPrice + '.00');
-    $('#pizzaMade').text(sizeInput + ' pizza with ' + totalToppings);
+    $('#payment').text('$ ' + total + '.00');
    });
  });
