e957ea0dfcf68f30bc0e3f810fed2e7d2e495a2e
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-01 17:49:35 -0700

    change a  universal variable below the event and removed unused code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 // Business Logic for PizzaPlace //
 
-//--- This is the OBJECT ---//
-const pizzaOrder = new Pizza();
-//--- This is a CONSTRUCTOR ---//
 function Pizza(size, topping1, topping2, topping3, topping4, topping5) {
 //--- These are PROPERTIES ---//
   this.size = size,
-  this.topping1 = topping1;
-  this.topping2 = topping2;
-  this.topping3 = topping3;
-  this.topping4 = topping4;
-  this.topping5 = topping5
+  this.topping1 = topping1,
+  this.topping2 = topping2,
+  this.topping3 = topping3,
+  this.topping4 = topping4,
+  this.topping5 = topping5,
+  this.price = 0
 
 }
 
 //--- This is PROTOTYPE (algorithm C = B + A)--- pizzaPrice = method
 Pizza.prototype.pizzaPrice = function(size, topping1, topping2, topping3, topping4, topping5) {
-  const totalPrice = size + topping1 + topping2 + topping3 + topping4 + topping5
-  return totalPrice
+  const totalPrice = size + topping1 + topping2 + topping3 + topping4 + topping5;
+  this.price = totalPrice;
+  console.log(this.price);
+
 }
 
 // User Interface Logic for Pizza Place //
@@ -29,22 +29,21 @@ $(document).ready(function() {
     const sizeInput = parseInt($('#pieSize').val());
     const artichoke   = $('#artichokes').is(':checked') ? 1 : 0;
     const olive     = $('#olives').is(':checked') ? 1 : 0;
-    const jalapeno  = $('#jalapenos').is(':checked') ? 1 : 0;
+    const jalepeno  = $('#jalepenos').is(':checked') ? 1 : 0;
     const tomato    = $('#tomatoes').is(':checked') ? 1 : 0;
     const cheese    = $('#cheese').is(':checked') ? 1 : 0;
+    const pizzaOrder = new Pizza(sizeInput, artichoke, olive, jalepeno, tomato, cheese);
 
-    console.log('artichoke=' + artichoke);
-    console.log('olive=' + olive);
-    console.log('jalapeno=' + jalapeno);
-    console.log('tomato=' + tomato);
-    console.log('cheese=' + cheese);
+    // console.log('artichoke=' + artichoke);
+    // console.log('olive=' + olive);
+    // console.log('jalepeno=' + jalepeno);
+    // console.log('tomato=' + tomato);
+    // console.log('cheese=' + cheese);
 
 //--- Calls on the METHOD ---//
-    const total = pizzaOrder.pizzaPrice(sizeInput, artichoke, olive, jalapeno, tomato, cheese);
-
-
+    const total = pizzaOrder.pizzaPrice(sizeInput, artichoke, olive, jalepeno, tomato, cheese);
 
     $('.output').show();
-    $('#payment').text('$ ' + total + '.00');
+    $('#payment').text('$ ' + pizzaOrder.price + '.00');
    });
  });
